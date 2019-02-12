1c9e6344875e57860b520a828b516d9381a59415
Jann C <janncabudoy@gmail.com>
2019-02-12 15:27:02 -0600

    Setting default window size

--- PyCite/PyCite.py
+++ PyCite/PyCite.py
@@ -1,8 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
+
 #Main window set up
+defaultWidth = 640
+defaultHeight = 640
+
 root = Tk()
 root.title("PyCite")
-
+mainWindow = ttk.Frame(master=root, width=defaultWidth, height=defaultHeight)
+mainWindow.pack()
+mainWindow.pack_propagate(0)
 root.mainloop()
