c66565b03861982ec04295ecdba627f299822474
Jann C <janncabudoy@gmail.com>
2019-02-12 20:56:29 -0600

    Input Fields complete

--- PyCite/PyCite.py
+++ PyCite/PyCite.py
@@ -1,13 +1,3 @@
-from tkinter import *
-from tkinter import ttk
+from ApplicationWindow import *
 
-
-#Main window set up
-defaultDims = '640x640'
-
-
-root = Tk()
-root.title("PyCite")
-root.geometry(defaultDims)
-
-root.mainloop()
+ApplicationWindow()
